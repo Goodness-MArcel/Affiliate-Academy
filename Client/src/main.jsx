c0d25affe675b74fe078b2f63b1298dea994dc7a
Johnjerry <johnjerry8749@gmail.com>
2025-11-05 04:08:24 -0800

    push to ghithub

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -22,8 +22,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './index.css'
 import App from './App.jsx'
-import { UserProvider } from './context/userContext.jsx'
-import { AdminProvider } from './context/AdminContext.jsx'  // ← NEW
+// import { UserProvider } from './context/userContext.jsx'
+// import { AdminProvider } from './context/AdminContext.jsx'  // ← NEW
 import { AuthProvider } from './context/AuthProvider.jsx'  // ← NEW
 
 createRoot(document.getElementById('root')).render(
